6decdc741fbbbc654120e5b01f647ccf56358044
Michael Johnson <michael@coder828.com>
2020-10-16 13:33:31 -0600

    added reload button

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Picture in Picture
 
-Cool feature of displaying a picture in picture using these methods:
-
-- [CSS Tricks Article](https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/)
+Cool feature of displaying a Picture in Picture using these methods:
 
 - [Screen Capture API](https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API)
 - [W3.org Picture in Picture](https://www.w3.org/TR/picture-in-picture/)
+
+### Browsers
+
+Scroll to the bottom of [this CSS Tricks article](https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/) to see which browsers support Picture in Picture.
--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+let videoLoaded = document.getElementById("videoLoaded");
 
+const videoElement = document.getElementById("video");
+const startButton = document.getElementById("startButton");
+const choosePictureButton = document.getElementById("choosePictureButton");
+
+// Prompt user to select media stream
+// pass that to video element
+// play video
+
+async function selectMediaStream() {
+  try {
+    videoLoaded.innerHTML = '<i class="fa fa-times"></i>';
+
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+
+    // once picture is chosen, show videoLoaded check mark
+    videoLoaded.innerHTML = '<i class="fa fa-check"></i>';
+  } catch (error) {
+    console.log("selectMediaStream", error);
+  }
+}
+
+// event listeners
+startButton.addEventListener("click", async () => {
+  // disable button
+  startButton.disabled = true;
+
+  // start picture in picture
+  const requestReady = await videoElement.requestPictureInPicture();
+
+  // reset button
+  startButton.disabled = false;
+});
+
+choosePictureButton.addEventListener("click", () => {
+  location.reload();
+  return false;
+});
+
+// on load
+selectMediaStream();
